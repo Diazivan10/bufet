c191315cc0e3c6993ca27a450ceec2d1e214412c
Ivan Diaz <irdiaz97@hotmail.com>
2021-04-17 11:10:57 -0500

    bufet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AbogadosComponent } from './abogados/abogados.component';
+import { AppComponent } from './app.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'nosotros',
+    component:NosotrosComponent
+  },
+  {
+  path: ' ',
+  component:AppComponent
+},
+{
+  path: 'abogados',
+  component:AbogadosComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { AbogadosComponent } from './abogados/abogados.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NosotrosComponent,
+    AbogadosComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
